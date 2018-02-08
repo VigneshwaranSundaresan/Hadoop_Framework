84c4d4fd088206b80803173f0fe9b99b2914fa37
VigneshwaranSundaresan <36016318+VigneshwaranSundaresan@users.noreply.github.com>
2018-02-07 23:48:43 -0600

    Add files via upload

---
+++ qualitychecks.py
@@ -0,0 +1,26 @@
+import os
+import csv
+import sys
+import pkgutil
+import subprocess
+import re
+from datetime import datetime,date,time, timedelta
+import logging
+import time
+import configparser
+from logger import logger
+
+class performQchecks():
+
+    def emptyfilecheck(self,logfile,source_file_path,source_file_name):
+
+        pass
+
+    def qccheck(self,logfile,source_file_path,source_qc_file):
+
+        pass
+
+    def identitycheck(self,logfile,source_file_path,source_file_name):
+
+        pass
+
